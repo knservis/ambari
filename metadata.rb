9d9626febff9fc47fab5424c8836494337dca9e3
Konstantinos Servis <kservis@kservis.dyn.syd.atlassian.com>
2015-02-20 18:44:22 +1100

    bump version

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             'ambari'
-maintainer       'JULIEN PELLET'
-maintainer_email 'chef@julienpellet.com'
+maintainer       'Konstantinos Servis'
+maintainer_email 'knservis@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures ambari'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.11'
+version          '0.1.12'
 supports 'redhat', ">= 5.0"
 supports 'centos', ">= 5.0"
 supports 'amazon', ">= 5.0"
